81f272266858dd921116d6e0667a69abd2a7a00e
nuggi46 <molinasmonica@gmail.com>
2023-10-28 19:40:27 -0300

    sextp commit

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ FUNCIONES PARA ALIMENTAR LA API
 
 #librerías
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 import pandas as pd
 
 
@@ -18,44 +17,6 @@ app = FastAPI()
 
 #dataframes que se utilizan en las funciones de la API
 tabla_final = pd.read_parquet("data/funcion_1.parquet")
-
-@app.get("/", response_class=HTMLResponse)
-async def incio ():
-    principal= """
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>API Steam</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    padding: 20px;
-                }
-                h1 {
-                    color: #333;
-                    text-align: center;
-                }
-                p {
-                    color: #666;
-                    text-align: center;
-                    font-size: 18px;
-                    margin-top: 20px;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>API de consultas sobre juegos de la plataforma Steam</h1>
-            <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
-            <p>INSTRUCCIONES:</p>
-            <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
-            <p>Consulte en el siguiente enlace:<a href="https://pi1-steamgames-deploy-jimefioni.onrender.com/docs/">{{FastAPI}}</a></p>
-            <p> El desarrollo de este proyecto esta en <a href="https://github.com/JimeFioni/PI_1-MLOps_Juegos_Steam"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
-            <p>María Jimena Fioni - 2023 -</p>
-        </body>
-    </html>
-
-        """    
-    return principal
 
 
 #Primera función
