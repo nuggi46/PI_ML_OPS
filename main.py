d57054a8a948f0dd538575af88fa7919c08650f1
nuggi46 <molinasmonica@gmail.com>
2023-10-29 16:36:58 -0300

    once commit

--- main.py
+++ main.py
@@ -39,11 +39,11 @@ async def UserForGenre(genero):
 
 @app.get("/UsersRecommend/{año}", name = "USERSRECOMMEND")
 async def UsersRecommend(año):
-    tabla1=max_reviews3[max_reviews3['year'] == int(año)]
+    tabla1=max_reviews3[max_reviews3['year'] == año]
     tabla1.reset_index()
     
-    dato = tabla1[tabla1["year"]== int(año)]["app_name"].iloc[0]
-    dato1 = tabla1[tabla1["year"]== int(año)]["app_name"].iloc[1]
-    dato2 = tabla1[tabla1["year"]== int(año)]["app_name"].iloc[2]
+    dato = tabla1[tabla1["year"]== año]["app_name"].iloc[0]
+    dato1 = tabla1[tabla1["year"]== año]["app_name"].iloc[1]
+    dato2 = tabla1[tabla1["year"]== año]["app_name"].iloc[2]
     
-    return {"Los juegos más recomendados para el año": int(año), "Puesto 1": dato,"Puesto 2": dato1,"Puesto 3": dato2}
+    return {"Los juegos más recomendados para el año": año, "Puesto 1": dato,"Puesto 2": dato1,"Puesto 3": dato2}
